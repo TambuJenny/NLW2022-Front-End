36f984a1a872da8fa26af4246476a7f62ef18727
Tambu Jenny <tambujenny@gmail.com>
2022-09-16 02:03:21 +0100

    Criando componentes e propriedades

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./styles/main.css";
 import logoImg from "./assets/Logo.svg";
-import  {MagnifyingGlassPlus} from 'phosphor-react';
+
+import { GameBanner } from "./components/GameBanner";
+import { CreatAdBanner } from "./components/CreatAdBanner";
 
 function App() {
+
+
   return (
+    
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 ">
       <img src={logoImg} alt="" />
 
@@ -16,74 +21,14 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-1.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24  botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              Legends of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative  rounded-lg overflow-hidden">
-          <img src="/game-2.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24 botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Dota 2 </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative  rounded-lg overflow-hidden">
-          <img src="/game-3.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24 botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              Counter Strike
-            </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative  rounded-lg overflow-hidden">
-          <img src="/game-4.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24 botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              Apex Legends{" "}
-            </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative  rounded-lg overflow-hidden">
-          <img src="/game-5.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24 botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Fortnite </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
-        <a href="" className="relative  rounded-lg overflow-hidden">
-          <img src="/game-6.png" alt="" />
-
-          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24 botttom-0 left-0 right-0">
-            <strong className="font-bold text-white block">WarCraft </strong>
-            <span className="text-zinc-300 text-sm block ">4 anúcios</span>
-          </div>
-        </a>
+       <GameBanner gameName="Grand Theft Auto V" imgUrl="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-188x250.jpg" ads={1} />
+       <GameBanner gameName="Grand Theft Auto V" imgUrl="https://static-cdn.jtvnw.net/ttv-boxart/33214-188x250.jpg" ads={5} />
+       <GameBanner gameName="Grand Theft Auto V" imgUrl="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-188x250.jpg" ads={5} />
+       <GameBanner gameName="NBA 2K23" imgUrl="https://static-cdn.jtvnw.net/ttv-boxart/772421245_IGDB-188x250.jpg" ads={0} />
       </div>
 
-      <div className="pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden ">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-          <strong className="text-2xl text-white font-black">Não encontrou seu duo ?</strong>
-          <span className="text-zinc-400 block">Publique un anúncio para encontrar novos players!</span>
-          </div>
-          <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-700 flex items-center gap-3">
-            <MagnifyingGlassPlus  size={24}/>
-            Publicar anúncio
-          </button>
-         </div>
-      </div>
+      <CreatAdBanner/>
+      
     </div>
   );
 }
---
+++ src/components/GameBanner.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Props {
+    gameName: string,
+    ads: Number,
+    imgUrl: string
+}
+export function GameBanner(props: Props) {
+
+    let ads = '';
+
+    if(props.ads > 1)
+         ads = props.ads +' anúcios';
+    else   
+        ads = props.ads +' anúcio';
+
+  return (
+    <>
+     <a href="" className="relative rounded-lg overflow-hidden">
+          <img src= {props.imgUrl} alt="" />
+
+          <div className="w-full pt-16 pb-12 px-4 bg-game-gradient absolute top-24  botttom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              {props.gameName}
+            </strong>
+            <span className="text-zinc-300 text-sm block "> {ads}</span>
+          </div>
+        </a>
+    </>
+  );
+}
